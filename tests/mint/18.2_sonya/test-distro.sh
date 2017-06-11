9c1ac20eaf3a49e73c2f72ac5aeab7b2c74c8ac0
Fabio Henrique Gonzales Barbosa <fbarbosa@atech.com.br>
2017-06-10 23:30:40 -0300

    Deleting generated files and fixing CI errors

--- tests/mint/18.2_sonya/test-distro.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-pwd=`pwd`/../../../
-volume="$pwd:/tmp/environment-setup"
-image_name="ansible-shutter-role"
-
-docker build -t $image_name . && docker run -v $volume --rm -i -t $image_name
